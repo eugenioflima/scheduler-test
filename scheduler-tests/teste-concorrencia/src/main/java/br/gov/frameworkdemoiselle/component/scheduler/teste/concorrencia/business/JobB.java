f549b1a8f15f79960a3ad654a7e07976d7d2b4f5
eugenioflima <eugenio.lima@serpro.gov.br>
2016-06-29 09:51:06 -0300

    Teste de reprodução do problema de concorrência do demoiselle-scheduler.

---
+++ scheduler-tests/teste-concorrencia/src/main/java/br/gov/frameworkdemoiselle/component/scheduler/teste/concorrencia/business/JobB.java
@@ -0,0 +1,33 @@
+package br.gov.frameworkdemoiselle.component.scheduler.teste.concorrencia.business;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import br.gov.frameworkdemoiselle.component.scheduler.teste.concorrencia.domain.Bookmark;
+import br.gov.frameworkdemoiselle.scheduler.lifecycle.Schedule;
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+
+@BusinessController
+public class JobB implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static int count;
+	
+	@Inject
+	private BookmarkBC bookmarkBC;
+	
+	@Schedule(cron = SchedulerUtils.DEFAULT_CRON)
+	public void execute() throws InterruptedException {
+		Thread.sleep(3000);
+		count++;
+		Bookmark bookmark = new Bookmark();
+		bookmark.setDescription("Teste " + String.format("%03d", count));
+		bookmark.setLink("host.com/test/" + count);
+		bookmarkBC.insert(bookmark);
+	}
+}
